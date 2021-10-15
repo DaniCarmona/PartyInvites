928afde9895296aad9636c54b99a02a7dd6d51d7
Daniel Carmona <carmona240101@hotmail.com>
2021-10-15 11:03:19 +0100

    Created post action to register guest's answers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,9 +28,17 @@ namespace PartyInvites.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(GuestResponse response)
+        {   
+            //TODO: Store guest response
+            return View("ThankYou");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
