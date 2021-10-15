8e107eb931532c41823198980ba9f2441a37fa20
Daniel Carmona <carmona240101@hotmail.com>
2021-10-15 12:07:58 +0100

    See a list of the guests

--- Models/Repository.cs
+++ Models/Repository.cs
@@ -7,7 +7,7 @@ namespace PartyInvites.Models
 {
     public class Repository
     {
-        private static List<GuestResponse> responses = new List<GuestResponse>;
+        private static List<GuestResponse> responses = new List<GuestResponse>();
 
         public static IEnumerable<GuestResponse> Responses => responses;
 
